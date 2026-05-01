c93a7c7e5ecb1cd594b8824ac32c21ca2d0fab7e
NicolasSKing <nocoben10@gmail.com>
2026-05-01 20:20:25 +0800

    here_first_i_created_the_variable_for_the_input_of_the_first_number_and_second_number_then_i_created_the_for_loop_the_will_run_all_the_function_of_the_calculator_first_if_the_choice_is_1_then_the_addition_function_from_the_class_will_run_the_same_as_all_the_other_math_funtions_then_else_if_the_input_is_invalid_then_it_will_print_a_statement_then_loop_and_will_ask_again_and_lastly_i_put_the_except_if_the_code_run_an_error_this_will_run_the_print_statement_inside_the_except_program

--- this_will_run_the_program.py
+++ this_will_run_the_program.py
@@ -16,7 +16,22 @@ try:
             print("Thank you for using this program")
             break
 
+        first_number = int(input("Enter first number: "))
+        second_number = int(input("Enter second number: "))
 
+        if choice == 1:
+          calculator.addition(first_number, second_number)
+        elif choice == 2:
+          calculator.subtract(first_number, second_number)
+        elif choice == 3:
+          calculator.multiplication(first_number, second_number)
+        elif choice == 4:
+          calculator.divition(first_number, second_number)
+        else:
+          print("Please enter a valid choice")
+
+except:
+    print("You have an ERROR LOL NOOB")
 
 
 
